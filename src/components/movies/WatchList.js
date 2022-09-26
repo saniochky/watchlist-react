f5d8214f18259fc4485f302c15bb8fa6359d0bc8
saniochky <sdubas40@gmail.com>
2022-09-26 23:49:41 +0300

    added animation to movie items on watchlist and watched pages

--- src/components/movies/WatchList.js
+++ src/components/movies/WatchList.js
@@ -1,4 +1,5 @@
 import {useContext} from 'react';
+import {motion} from 'framer-motion';
 import List from '../ui/List';
 import MovieItem from './items/MovieItem';
 import LoadingIndicator from '../ui/LoadingIndicator';
@@ -17,22 +18,36 @@ const WatchList = () => {
     if (moviesCtx.watchlistCount > 0) {
         content = (
             <List>
-                {moviesCtx.watchlist.map(movie => (
-                    <MovieItem
+                {moviesCtx.watchlist.map((movie, i) => (
+                    <motion.div
                         key={movie.id}
-                        movieId={movie.id}
-                        leftIcon={faThumbsUp}
-                        rightIcon={faThumbsDown}
-                        leftIconTitle="Like"
-                        rightIconTitle="Dislike"
-                        leftAction={moviesCtx.likeMovie}
-                        rightAction={moviesCtx.dislikeMovie}
-                        buttonName="Remove"
-                        buttonTitle="Remove movie from Watchlist"
-                        buttonHoverColor="red"
-                        buttonBacklightHoverColor="RED"
-                        buttonClickHandler={moviesCtx.removeMovieFromWatchlist}
-                    />
+                        variants={{
+                            hidden: {opacity: 0, y: 50},
+                            visible: (i) => ({
+                                opacity: 1,
+                                y: 0,
+                                transition: {delay: i * 0.1}
+                            }),
+                        }}
+                        initial="hidden"
+                        animate="visible"
+                        custom={i}
+                    >
+                        <MovieItem
+                            movieId={movie.id}
+                            leftIcon={faThumbsUp}
+                            rightIcon={faThumbsDown}
+                            leftIconTitle="Like"
+                            rightIconTitle="Dislike"
+                            leftAction={moviesCtx.likeMovie}
+                            rightAction={moviesCtx.dislikeMovie}
+                            buttonName="Remove"
+                            buttonTitle="Remove movie from Watchlist"
+                            buttonHoverColor="red"
+                            buttonBacklightHoverColor="RED"
+                            buttonClickHandler={moviesCtx.removeMovieFromWatchlist}
+                        />
+                    </motion.div>
                 ))}
             </List>
         );
--- src/components/movies/WatchedMovieList.js
+++ src/components/movies/WatchedMovieList.js
@@ -1,4 +1,5 @@
 import {useRef, useContext} from 'react';
+import {motion} from 'framer-motion';
 import List from '../ui/List';
 import MovieItem from './items/MovieItem';
 import LoadingIndicator from '../ui/LoadingIndicator';
@@ -24,22 +25,37 @@ const WatchedMovieList = () => {
     if (moviesCtx.watchedCount > 0) {
         content = (
             <List>
-                {moviesCtx.watched.map(movie => (
-                    <MovieItem
+                {moviesCtx.watched.map((movie, i) => (
+                    <motion.div
                         key={movie.id}
-                        ref={el => itemsRef.current[movie.id] = el}
-                        movieId={movie.id}
-                        flippable
-                        flipUpCards={flipUpCardsHandler}
-                        leftIcon={faMagnifyingGlass}
-                        rightIcon={faCircleXmark}
-                        leftIconTitle="View details"
-                        rightIconTitle="Remove movie from the Watched"
-                        rightAction={moviesCtx.removeMovieFromWatched}
-                        buttonName="Watch"
-                        buttonTitle={"Rewatch movie"}
-                        buttonClickHandler={() => window.open('https://rezka.ag/', '_blank', 'noopener,noreferrer')}
-                    />
+                        variants={{
+                            hidden: {opacity: 0, y: 50},
+                            visible: (i) => ({
+                                opacity: 1,
+                                y: 0,
+                                transition: {delay: i * 0.1}
+                            }),
+                        }}
+                        initial="hidden"
+                        animate="visible"
+                        custom={i}
+                    >
+                        <MovieItem
+                            key={movie.id}
+                            ref={el => itemsRef.current[movie.id] = el}
+                            movieId={movie.id}
+                            flippable
+                            flipUpCards={flipUpCardsHandler}
+                            leftIcon={faMagnifyingGlass}
+                            rightIcon={faCircleXmark}
+                            leftIconTitle="View details"
+                            rightIconTitle="Remove movie from the Watched"
+                            rightAction={moviesCtx.removeMovieFromWatched}
+                            buttonName="Watch"
+                            buttonTitle={"Rewatch movie"}
+                            buttonClickHandler={() => window.open('https://rezka.ag/', '_blank', 'noopener,noreferrer')}
+                        />
+                    </motion.div>
                 ))}
             </List>
         );
